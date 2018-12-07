3b128c9470090e27bc74cadee28987e1e59a17d6
Marwan SENTENAC <m.sentenac@gmail.com>
2018-12-07 12:21:53 +0100

    config/db for heroku

--- config/db.js
+++ config/db.js
@@ -1,15 +1,21 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-})
+let sequelize = null
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL)
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  })
+}
 
 module.exports = sequelize
