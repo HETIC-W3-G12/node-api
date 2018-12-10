8649a9ea43612f4d7f96741ae2cac6ed407e563e
Marwan SENTENAC <m.sentenac@gmail.com>
2018-12-10 01:53:14 +0100

    add some default config values

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'euko-api',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres'
   },
   production: {
