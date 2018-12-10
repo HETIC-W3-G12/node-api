1e0b5349da79a9d7f5b34838c40711cb17f498da
Marwan SENTENAC <m.sentenac@gmail.com>
2018-12-10 02:12:34 +0100

    rm static homepage

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
@@ -14,7 +14,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 db.sequelize
   .authenticate()
