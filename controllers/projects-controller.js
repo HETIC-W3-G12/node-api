1d70e593ef3f376c1722213c4756c774646d98b2
samantha <samantha@gotoandbuzz.com>
2019-01-10 16:43:31 +0100

    add create Project routes and update API doc

--- controllers/projects-controller.js
+++ controllers/projects-controller.js
@@ -6,6 +6,8 @@ class ProjectsController extends AppController {
     this.index = this.index.bind(this)
     this.create = this.create.bind(this)
     this.getOneProject = this.getOneProject.bind(this)
+    this.projectFounded = this.projectFounded.bind(this)
+    this.getProjectByUser = this.getProjectByUser.bind(this)
   }
 
   /* list of the project */
@@ -21,20 +23,38 @@ class ProjectsController extends AppController {
 
   /* get details of one project */
   getOneProject(req, res) {
-    this.Project.findAll({
-      where: {
-        id: req.params.id
-      }
+    this.Project.find({
+      where: { id: req.params.id }
    }).then(projects => {
       res.json(projects)
     })
   }
 
+  /* create a new project */
   create(req, res) {
-    this.Project.create(req.body).then(project => {
-      res.json(project)
-    })
+    this.Project.create(req.body)
+      .then(project => {
+        res.json(project)
+      })
+      .catch(function (err) {
+        res.json(err)
+      })
   }
+
+  /* update state of the project to running (has been founded) */
+  projectFounded(req,res){
+    eq.params.id
+
+    this.Project.create(req.body)
+      .then(project => {
+        res.json(project)
+      })
+      .catch(function (err) {
+        res.json(err)
+      })
+  }
+
+  getProjectByUser(req,res){}
 }
 
 module.exports = new ProjectsController
