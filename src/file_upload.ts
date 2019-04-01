68bcdae6e7c5c0af6e387558c66af077b1062a60
Marwan SENTENAC <m.sentenac@gmail.com>
2019-04-01 15:58:57 +0200

    uploadFile promise

--- src/file_upload.ts
+++ src/file_upload.ts
@@ -12,31 +12,41 @@ AWS.config.update({ region: 'eu-west-3' })
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
 export const uploadFile = (file = '', ACL = 'private') => {
-  s3.upload({ ACL, Bucket: bucketParams.Bucket, Body: file, Key: uuid() }, (err, data) => {
-    if (err) {
-      console.log('Error', err)
-    }
-    if (data) {
-      console.log('Upload Success', data.Location)
-    }
+  return new Promise((resolve, reject) => {
+    s3.upload(
+      { ACL, Bucket: bucketParams.Bucket, Body: file, Key: uuid() },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        if (data) {
+          resolve(data)
+        }
+      }
+    )
   })
 }
 
-export const getFile = (Key) => {
+export const getFile = Key => {
   return new Promise((resolve, reject) => {
-    s3.getObject({Bucket: bucketParams.Bucket, Key}, (err, data) => {
-      if (err) reject(err.stack) // an error occurred
-      else     resolve(data)     // successful response
+    s3.getObject({ Bucket: bucketParams.Bucket, Key }, (err, data) => {
+      if (err) reject(err.stack)
+      // an error occurred
+      else resolve(data) // successful response
     })
   })
 }
 
-// uploadFile('ça marche', 'public-read')
-getFile('a89e6880-5463-11e9-845c-db205f68e499').then(resp => {
-  console.log(resp)
-}).catch(err => {
-  console.log(err)
-})
+// uploadFile('ça marche', 'public-read').then(resp => {
+//   console.log(resp)
+// })
+// getFile('06db7fb0-5486-11e9-8a24-e36ffb5f701b')
+//   .then(resp => {
+//     console.log(resp)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 
 const createBucket = () => {
   console.log('Creating bucket...')
