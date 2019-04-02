0c1ab8f72ad5679dfd3f6729339ddf506d8ee55a
Marwan SENTENAC <m.sentenac@gmail.com>
2019-04-02 15:55:50 +0200

    file-test catch err

--- src/routes/file-test.ts
+++ src/routes/file-test.ts
@@ -6,12 +6,16 @@ import { getFile, uploadFile } from '../file_upload'
 router.post('/', (req, res) => {
   uploadFile(req.body.file, req.body.name).then(resp => {
     res.json(resp)
+  }).catch(err => {
+    res.json(err)
   })
 })
 
 router.get('/:uuid', (req, res) => {
   getFile(req.params.uuid).then(resp => {
     res.json(resp)
+  }).catch(err => {
+    res.json(err)
   })
 })
 
