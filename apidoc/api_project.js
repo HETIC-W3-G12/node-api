89c84ba700203b27e0162e402532926a5f3d96bc
Marwan SENTENAC <m.sentenac@gmail.com>
2019-05-29 01:08:26 +0200

    regen doc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,17 +1,1 @@
-define({
-  "name": "EUKO API documentation",
-  "version": "1.0.0",
-  "description": "EUKO's API documentation",
-  "template": {
-    "forceLanguage": "en"
-  },
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-05-28T22:48:59.484Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
-  }
-});
+define({  "name": "EUKO API documentation",  "version": "1.0.0",  "description": "EUKO's API documentation",  "template": {    "forceLanguage": "en"  },  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-05-28T23:08:16.933Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
